d452142c27715e55670b96bfb1f7008164d10621
Jakub <wetido@gmail.com>
2020-04-11 02:17:26 +0200

    added toolbar and moved buttons functions to toolbar menu items

--- app/src/main/java/com/example/nutritionscanner/MainActivity.java
+++ app/src/main/java/com/example/nutritionscanner/MainActivity.java
@@ -1,59 +1,65 @@
 package com.example.nutritionscanner;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 
 import android.content.Intent;
 
 import android.os.Bundle;
 
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.TextView;
-
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    Button scan;
-    Button add;
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        scan = (Button) findViewById(R.id.qrScan);
-        scan.setText("SQ SCAN");
-
-        add = findViewById(R.id.add);
-        add.setText("Adding elements");
-
-
-        scan.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                Intent intent = new Intent("com.google.zxing.client.android.SCAN");
-                intent.putExtra("SCAN_MODE", "QR_CODE_MODE"); // "PRODUCT_MODE for bar codes
-                startActivityForResult(intent, 0);
-
-            }
-        });
-
-        add.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                Intent intent = new Intent(getApplicationContext(), AddingDataActivity.class);
-                intent.putExtra("barcode", ""); // "PRODUCT_MODE for bar codes
-                startActivityForResult(intent, 2);
-
-            }
-        });
+        toolbar = findViewById(R.id.this_toolbar);
+        setSupportActionBar(toolbar);
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.item1:
+                Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+                intent.putExtra("SCAN_MODE", "QR_CODE_MODE"); // "PRODUCT_MODE for bar codes
+                startActivityForResult(intent, 0);
+                return true;
+
+            case R.id.item2:
+                Intent intent2 = new Intent(getApplicationContext(), AddingDataActivity.class);
+                intent2.putExtra("barcode", ""); // "PRODUCT_MODE for bar codes
+                startActivityForResult(intent2, 2);
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
